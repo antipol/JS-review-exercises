e49404c4d5cd21ebf82fb5a9ecd18a20d967c911
antipol <trine_54@hotmail.com>
2019-07-09 11:47:47 +0200

    updating with solutions to the rest of the exercises

--- spread-operator/solutions.js
+++ spread-operator/solutions.js
@@ -30,24 +30,35 @@ console.log(combineTwoArrays([1, 3, 'dog'], [1, 'elephant']));
 
 
 // 5. Sum every other
-const sumEveryOther = (...args) => [...args].filter((num, i) => num[i]);
+//Filtering every other index number, reduce every other num to one sum
+const sumEveryOther = (...args) => [...args].filter((num, i) => i % 2 === 0).reduce((acc, cur) => acc + cur);
+
 console.log(sumEveryOther(2, 2, 3, 4, 6, 10));
+//11
+//(2 + 3 + 6)
 
 
+//6. only uniques
+const onlyUniques = (...args) => {
+  const uniqueArr = [];
+  args.forEach(item => uniqueArr.includes(item) ? uniqueArr : uniqueArr.push(item))
+  return uniqueArr;
+}
+
+console.log(onlyUniques('cat', 'cat', 'dog', 'pig'));
+//[ 'cat', 'dog', 'pig' ]
+console.log(onlyUniques(1, 4, 7, 1, 2, 7, 4));
+//[ 1, 4, 7, 2 ]
 
 
+//7. Combine all arrays
+const combineAllArrays = (...arrays) => [].concat(...arrays)
+
+console.log(combineAllArrays([2, 6, 71, -100], ['cat', true, 23], [false, 234]));
 
 
+//8. Square and sum
+const squareAndSum = (...args) => args.map(item => item * item).reduce((acc, cur) => acc + cur);
 
-
-
-
-
-
-
-
-
-
-
-
-//
+console.log(squareAndSum(2, 5, 10));
+//129
